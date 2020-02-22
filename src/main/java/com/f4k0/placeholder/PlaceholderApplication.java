62f0346634227e1a34bfc3debee366565bd3689a
Erik Fabian <fabianerik95@gmail.com>
2020-02-22 20:49:48 +0100

    PLCH-5 - Add Spring WebFlux configuration

--- src/main/java/com/f4k0/placeholder/PlaceholderApplication.java
+++ src/main/java/com/f4k0/placeholder/PlaceholderApplication.java
@@ -2,8 +2,10 @@ package com.f4k0.placeholder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class PlaceholderApplication {
 
 	public static void main(String[] args) {
